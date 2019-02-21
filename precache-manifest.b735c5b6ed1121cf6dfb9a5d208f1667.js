cb54da51a1e814da737c0b5d0eae19ffe11d5332
naffiq <abdu.galymzhan@gmail.com>
2019-02-21 14:24:11 +0100

    Updates

---
+++ precache-manifest.b735c5b6ed1121cf6dfb9a5d208f1667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf81345dc135f16d6518",
+    "url": "/react-pwa/static/js/runtime~main.bf81345d.js"
+  },
+  {
+    "revision": "2bc83ca00bf78aa7daae",
+    "url": "/react-pwa/static/js/main.2bc83ca0.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-pwa/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "2bc83ca00bf78aa7daae",
+    "url": "/react-pwa/static/css/main.a674b219.chunk.css"
+  },
+  {
+    "revision": "1755ee040cb152cc02435632516bf89e",
+    "url": "/react-pwa/index.html"
+  }
+];
